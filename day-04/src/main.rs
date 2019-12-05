04606e0d740ec5edafe91ee5245cfd06740608f0
Bernd Kaiser <bk@dfjk.eu>
2019-12-05 16:55:12 +0100

    day-04: uncomment part 1

--- day-04/src/main.rs
+++ day-04/src/main.rs
@@ -6,12 +6,12 @@ const START: usize = 136_818;
 const END: usize = 685_979;
 
 fn main() {
-    // let valid_passwords: usize = (START..END)
-    //     .map(verify_password)
-    //     .filter_map(Result::ok)
-    //     .count();
+    let valid_passwords: usize = (START..END)
+        .map(verify_password)
+        .filter_map(Result::ok)
+        .count();
 
-    // println!("Valid passwords 1): {}", valid_passwords);
+    println!("Valid passwords 1): {}", valid_passwords);
 
     let valid_passwords_2: usize = (START..END)
         .map(verify_password_2)
